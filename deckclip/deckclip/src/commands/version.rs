3314f0a5cfa18586ff8e431cb425fb202243f27d
Yuze Pan <panyuzepan6@gmail.com>
2026-04-11 15:01:29 +0800

    feat: 添加libc依赖并优化版本命令输出的终端宽度检测

--- deckclip/deckclip/src/commands/version.rs
+++ deckclip/deckclip/src/commands/version.rs
@@ -3,11 +3,24 @@ use serde_json::json;
 
 const LOGO: &str = include_str!("../logo.ans");
 
+fn terminal_width() -> Option<u16> {
+    unsafe {
+        let mut ws: libc::winsize = std::mem::zeroed();
+        if libc::ioctl(libc::STDOUT_FILENO, libc::TIOCGWINSZ, &mut ws) == 0 && ws.ws_col > 0 {
+            Some(ws.ws_col)
+        } else {
+            None
+        }
+    }
+}
+
 pub fn run(output: OutputMode) {
     let version = env!("CARGO_PKG_VERSION");
     match output {
         OutputMode::Text => {
-            print!("{}", LOGO);
+            if terminal_width().unwrap_or(80) >= 50 {
+                print!("{}", LOGO);
+            }
             println!("deckclip {}", version);
         }
         OutputMode::Json => {
